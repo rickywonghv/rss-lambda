0c1952a528e57446201fdd1cda309aa4fc97ec94
damonwonghv <damonwcw@outlook.com>
2018-09-15 20:26:39 +0800

    Cancel other podcast and update xml response

--- app.js
+++ app.js
@@ -16,23 +16,14 @@ var cmm="http://feeds.feedburner.com/cmmp14";
 var warrior="http://www.ourradio.hk/index.php/radio-program/talk/warrior";
 
 exports.handler=function(event,context,cb){
-	var digi="http://www.ourradio.hk/index.php/radio-program/tech/digi";
-    var mystery="http://www.ourradio.hk/index.php/radio-program/newage/mystery";
-    if(event.code=="mystery"){
-      var url=mystery;
+//	var digi="http://www.ourradio.hk/index.php/radio-program/tech/digi";
+    var url="http://www.ourradio.hk/index.php/radio-program/newage/mystery";
+      //var url=mystery;
       var cat="mystery";
       var desc="mystery";
       var title="無奇不有";
       var img="https://scontent-hkg3-1.xx.fbcdn.net/v/t1.0-1/p200x200/10353716_720428991363725_2869408499533067513_n.jpg?oh=0833fe19516f2438f8286ac229543b41&oe=591546F5";
-    }else if (event.code=="digi") {
-      var url=digi;
-      var cat="Technology";
-      var desc="數碼生活頻道";
-      var title="數碼生活頻道";
-      var img="https://pbs.twimg.com/profile_images/768772073787228160/o8FxLsPs_400x400.jpg";
-    }else{
-      context.error("error",null);
-    }
+    
 
     var feed = new RSS({
         title: title,
@@ -55,10 +46,6 @@ exports.handler=function(event,context,cb){
         var a=0;
         var now;
         var rss;
-
-
-
-
         do{
             now='.leading-'+a;
             $(now).filter(function(){
@@ -80,8 +67,16 @@ exports.handler=function(event,context,cb){
         }while(a<=6){
             var xml = feed.xml();
             //writerss(rssfile,xml);
-            cb(null,xml);
-            //console.log("RSS Feed updated");
+            //cb(null,xml);
+            cb(null,{
+              statusCode: 200,
+              headers: {
+                 //"Content-Type" : "text/html"
+                 "Content-Type" : "application/xml"
+              },
+              body: xml
+            })
+            console.log("RSS Feed updated");
         }
 
     });
